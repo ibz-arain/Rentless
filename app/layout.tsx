1967e4d5cf688dc7a5730d838abe43e150746f57
Ibrahim Arain <ibrahimarain04@gmail.com>
2026-09-06 17:48:40 -0400

    NextJS Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
@@ -18,12 +18,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Rentless",
   description: "Relentless",
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
